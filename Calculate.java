99c44e0652ace160c1a8fb6b570889ca29d038d3
Desmoond <artur02288@yandex.ru>
2018-12-17 23:28:26 +0300

    #1 Update Calculate Sum/div/mod/*/^ Lesson 1

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,22 @@
 public class Calculate {
-	public static void main(String[] args) {
+	public static void main(String[] arg) {
 		System.out.println("Hello world!!!");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+
+		int res = first + second;
+		System.out.println("res (+) " + res);
+
+		res = first/second;
+		System.out.println("res (/) " + res);
+
+		res = first%second;
+		System.out.println("res (%) " + res);
+
+		res = first*second;
+		System.out.println("res (*) " + res);
+
+		res = (int)Math.pow(first,second);
+		System.out.println("res (^ "+second +") " + res);
 	}
 }
